8caed19670480863d4bde3b86e2f3b255c6482b6
Christian <christianoliveira1908@hotmail.com>
2022-01-26 13:29:50 -0200

    Aula 03 - Realização da listagem das mensagens

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,8 +1,182 @@
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React, { useState } from 'react';
+import appConfig from '../config.json';
+
+
 const Chat = () => {
+    const [message, setMessage] = useState('');
+    const [listMessages, setListMessages] = useState([]);
+
+    const handleMessage = event => {
+        setMessage(event.target.value);
+    }
+
+    const handleKeyPressMessage = event => {
+
+        if (event.charCode === 13) {
+            const newMessage = {
+                id: listMessages.length + 1,
+                from: 'zNexTage',
+                text: message
+            }
+
+            event.preventDefault();
+            setListMessages([newMessage, ...listMessages]);
+            setMessage('');
+        }
+    }
+
     return (
-        <div>
-            Página do chat
-        </div>
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary['400'],
+                backgroundImage: 'url(https://w.wallha.com/ws/12/7TD08dxC.jpg)',
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+
+
+                    <MessageList
+                        messages={listMessages}
+                    />
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            onChange={handleMessage}
+                            onKeyPress={handleKeyPressMessage}
+                            value={message}
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+const Header = () => {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+const MessageList = ({ messages }) => {
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'auto',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {messages.map(message => (
+                <Text
+                    key={message.id}
+                    tag="li"
+                    styleSheet={{
+                        borderRadius: '5px',
+                        padding: '6px',
+                        marginBottom: '12px',
+                        hover: {
+                            backgroundColor: appConfig.theme.colors.neutrals[700],
+                        }
+                    }}
+                >
+                    <Box
+                        styleSheet={{
+                            marginBottom: '8px',
+                        }}
+                    >
+                        <Image
+                            styleSheet={{
+                                width: '20px',
+                                height: '20px',
+                                borderRadius: '50%',
+                                display: 'inline-block',
+                                marginRight: '8px',
+                            }}
+                            src={`https://github.com/${message.from}.png`}
+                        />
+                        <Text tag="strong">
+                            {message.from}
+                        </Text>
+                        <Text
+                            styleSheet={{
+                                fontSize: '10px',
+                                marginLeft: '8px',
+                                color: appConfig.theme.colors.neutrals[300],
+                            }}
+                            tag="span"
+                        >
+                            {(new Date().toLocaleDateString())}
+                        </Text>
+                    </Box>
+                    {message.text}
+                </Text>
+            ))}
+        </Box>
     )
 }
 
